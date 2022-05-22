3dc23004f68206e11ef7873941e5cb41a7bbc603
bgunyel <bgunyel@gmail.com>
2022-05-22 14:36:35 +0300

    Constants file added

---
+++ constants.py
@@ -0,0 +1,11 @@
+from typing import Final
+
+DATA_FOLDER: Final = './data/'
+OUT_FOLDER: Final = './out/'
+
+EPIAS_FOLDER: Final = DATA_FOLDER + 'epias/'
+SLIDING_HOLIDAYS_JSON = DATA_FOLDER + 'sliding_holidays.json'
+
+CONSUMPTION: Final = 'Consumption (MWh)'
+HOLIDAY: Final = 'holiday'
+
